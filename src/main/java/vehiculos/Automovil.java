ec96e4cbdfbe011ad246d49a443825be00258487
alvarojguerrero <guerreroalvarojavier@gmail.com>
2021-11-30 20:52:53 -0500

    Agregando las clases y metodos

---
+++ src/main/java/vehiculos/Automovil.java
@@ -0,0 +1,25 @@
+package vehiculos;
+
+public class Automovil extends Vehiculo {
+
+    public static int cantidadAutomoviles;
+    private int puestos;
+    
+    public Automovil() {
+    }
+
+    public Automovil(String placa, String nombre, int precio, int peso, Fabricante fabricante, int puestos) {
+        super(placa, 4, 100, nombre, precio, peso, "FWD", fabricante);
+        this.puestos = puestos;
+        cantidadAutomoviles++;
+    }
+
+    public int getPuestos() {
+        return puestos;
+    }
+
+    public void setPuestos(int puestos) {
+        this.puestos = puestos;
+    }
+    
+}
